3eef61c11ac1b422c5f5d0ab2f538e71890810b7
Vander Gerald <vander.gerald@ui.ac.id>
2024-04-25 01:34:49 +0700

    Simulation slow subscriber.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
 use std::{thread, time};
 
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
-
 pub struct UserCreatedEventMessage {
     pub user_id: String,
     pub user_name: String
@@ -17,7 +16,7 @@ UserCreatedHandler {
     fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
         println!("In Vander's Computer [1906350603]. Message received: {:?}", message);
         Ok(())
     }
@@ -25,8 +24,7 @@ UserCreatedHandler {
 
 fn main() {
     let listener =
-        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:567
-            2".to_owned()).unwrap();
+        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
     _ = listener.listen("user_created".to_owned(),
         UserCreatedHandler{}, crosstown_bus::QueueProperties {
             auto_delete: false, durable: false, use_dead_letter: true 
